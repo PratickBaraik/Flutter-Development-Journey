fc24519efb2e8d7446cceaa8a6315b501cea85cb
PratickB111 <animeNerd.int@protonmail.com>
2023-12-24 23:24:24 +0530

    Adding a simple dart program

---
+++ dart_basics/first.dart
@@ -0,0 +1,19 @@
+void main() {
+  print("hello world");
+
+  // calling the function
+  calculate();
+}
+
+// user defined dart function
+void calculate() {
+  // initialization of some variables
+  var first_number = 10;
+  var second_number = 20;
+
+// computing total
+  var total_of_numbers = first_number + second_number;
+
+// printing the total
+  print("Total of two numbers: " + total_of_numbers.toString());
+}
