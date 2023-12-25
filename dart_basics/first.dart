3a2fff4ea43ac1f1c88a3fe2c694dfb9f4592e2d
PratickB111 <animeNerd.int@protonmail.com>
2023-12-25 22:56:13 +0530

    adding some dart code which shows use of array, normal intergers, double, string

--- dart_basics/first.dart
+++ dart_basics/first.dart
@@ -3,6 +3,9 @@ void main() {
 
   // calling the function
   calculate();
+
+  // calling the myfunction
+  myFunction();
 }
 
 // user defined dart function
@@ -16,4 +19,18 @@ void calculate() {
 
 // printing the total
   print("Total of two numbers: " + total_of_numbers.toString());
+}
+
+// function which contains different types of variables
+void myFunction() {
+  var myName = 'Pratick'; // my name
+  var myId = 1167; // my id
+  var PI = 3.14; // value of pi
+  var myArray = ['Calculator', 'Rular', 'Pencil', 'Laptop', 'Note book'];
+
+  // printing all details
+  print("My name: " + myName); // printing my name
+  print("My id: " + myId.toString()); // printing my id
+  print("Value of PI: " + PI.toString()); // printing the value of PI
+  print("Elements of array: " + myArray.join(", "));
 }
