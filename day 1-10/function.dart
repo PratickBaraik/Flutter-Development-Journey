5ac3ca48056cd5bea8626236349945f77fa7f2d5
PratickB111 <animeNerd.int@protonmail.com>
2024-02-26 22:08:55 +0530

    parameterized function code/lines

--- day 1-10/function.dart
+++ day 1-10/function.dart
@@ -3,6 +3,14 @@
 void main() {
   // printing and calling the user defined function
   print("My name is ${myName()}"); // using interpolation to call the function
+
+  // calling the function with parameters
+  myStore("Calender", 70, 27);
+  print("\n");
+  myStore("Gift cards", 30, 15);
+  print("\n");
+  myStore("Stove", 7, 300);
+  print("\n");
 }
 
 // my own function
@@ -10,4 +18,13 @@ String myName() {
   String name = "Pratick Baraik";
   // returning the name
   return name;
+}
+
+// function with arguments
+void myStore(String item, int price, int quantity) {
+  // statements to show details
+  print("Name: $item");
+  print("Price: $price");
+  print("Number of items: $quantity");
+  print("Total amount: ${price * quantity}");
 }
