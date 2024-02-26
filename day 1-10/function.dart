dc65d437a3bf922c66bd135bbacd89ad4c1637a9
PratickB111 <animeNerd.int@protonmail.com>
2024-02-26 22:02:20 +0530

    adding the function dart file

---
+++ day 1-10/function.dart
@@ -0,0 +1,13 @@
+//starting the program
+// default main function
+void main() {
+  // printing and calling the user defined function
+  print("My name is ${myName()}"); // using interpolation to call the function
+}
+
+// my own function
+String myName() {
+  String name = "Pratick Baraik";
+  // returning the name
+  return name;
+}
