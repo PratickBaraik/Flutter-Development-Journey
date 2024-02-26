de1f52c02ec5e8af6ee36347da912ef5e8b741bc
PratickB111 <animeNerd.int@protonmail.com>
2024-02-26 22:22:52 +0530

    adding the pattern dart file

---
+++ day 1-10/patterns.dart
@@ -0,0 +1,16 @@
+// starting the program
+void main() {
+  print("Use of patterns");
+  var (id, name, available) =
+      myFunction(10, "Perfume", true); // declaration of patter
+  // it keeps all the parameters stored in a var data type
+  // it can be printed in following ways
+  print("Id: $id");
+  print("Name: $name");
+  print("Is available: $available");
+}
+
+// using records of dart to create function with multiple data type
+(int, String, bool) myFunction(int id, String name, bool avail) {
+  return (id, name, avail); // returning more than one value
+}
