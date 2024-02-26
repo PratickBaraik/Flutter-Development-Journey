fc17574f1566dbfbebe05843f3a0a470b2acad7f
PratickB111 <animeNerd.int@protonmail.com>
2024-02-26 22:35:19 +0530

    adding the arrow function program of dart

---
+++ day 1-10/arrowFunction.dart
@@ -0,0 +1,15 @@
+// starting the program
+void main() {
+  // demonstrating the use of arrow function
+  // calling the function
+  print("My name is ${myName()}");
+  print("Value is ${value()}");
+  print("Value of PI is ${PI()}");
+}
+
+// defining an arrow function
+String myName() => "Pratick Baraik";
+
+int value() => 133321;
+
+double PI() => 3.14;
