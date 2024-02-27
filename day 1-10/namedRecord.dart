aa8e341ac6d0a2da41869d3d73a788194459d451
PratickB111 <animeNerd.int@protonmail.com>
2024-02-27 23:54:06 +0530

    adding dart code for named record demonstration

---
+++ day 1-10/namedRecord.dart
@@ -0,0 +1,7 @@
+
+void main() {
+  var person = ('Pratick', age: 23, isStudent: false);
+  print(person.$1); // Prints 'Pratick'
+  print(person.age); // Prints 23
+  print(person.isStudent); // Prints false
+}
