14aa81c145c41d17944333f1f8ca7c9c91dfe915
PratickB111 <animeNerd.int@protonmail.com>
2024-03-09 16:04:40 +0530

    adding code for lexical scope dart program

--- day 1-10/lexicalScope.dart
+++ day 1-10/lexicalScope.dart
@@ -1,6 +1,31 @@
 // starting the program
 
-var myNum = null;
+import 'dart:io';
+
+var myNum = null; // this variable has the lexical scope
 void main() {
-  print(myNum);
+  // taking input for main function
+  print("\nEnter value for main function: ");
+  myNum = int.parse(stdin.readLineSync()!);
+  print("For main funciton num = $myNum");
+
+  void innerFunc() {
+    // taking input for inner function
+    print("\nEnter value for inner function: ");
+    myNum = int.parse(stdin.readLineSync()!);
+    print("Now for inner function num = $myNum");
+  }
+
+  // calling the inner function
+  innerFunc();
+
+  // calling the outer function
+  outerFunc();
+}
+
+void outerFunc() {
+  // taking input for outer function
+  print("\nEnter value for outer function: ");
+  myNum = int.parse(stdin.readLineSync()!);
+  print("Now for outer function num = $myNum");
 }
