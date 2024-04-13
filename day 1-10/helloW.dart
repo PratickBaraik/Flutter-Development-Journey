1143046d3b4825f3e2133c8b82864c77c4823e5c
PratickB111 <animeNerd.int@protonmail.com>
2024-04-13 21:46:12 +0530

    revising the code

--- day 1-10/helloW.dart
+++ day 1-10/helloW.dart
@@ -3,4 +3,10 @@
 void main() {
   // printing the famous hello world statement
   print("Hello World");
+
+  var firstMessage = "Hello ";
+  var secondMessage = "World";
+
+  // printing both messages
+  print("$firstMessage$secondMessage"); // gives "Hello World"
 }
