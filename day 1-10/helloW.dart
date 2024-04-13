ca1835393f382b6780175a9e24da2ebcb420c56a
PratickB111 <animeNerd.int@protonmail.com>
2024-04-13 21:51:02 +0530

    added some more code

--- day 1-10/helloW.dart
+++ day 1-10/helloW.dart
@@ -9,4 +9,9 @@ void main() {
 
   // printing both messages
   print("$firstMessage$secondMessage"); // gives "Hello World"
+
+  // printing values other than string like number
+  print(10);
+  print(2.34);
+  print(1101); // doesn't require the use of double inverted commas to print message 
 }
