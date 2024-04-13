9bae2d9245132f8ff1f571baa8af4625d66a1d80
PratickB111 <animeNerd.int@protonmail.com>
2024-04-13 22:03:09 +0530

    doing operations on string

--- day 1-10/Operators.dart
+++ day 1-10/Operators.dart
@@ -6,4 +6,11 @@ void main() {
   print(3 * 4);
   print(8 / 3);
   print(3 % 2);
+
+  // doing operations on string
+  print('20' + '10'); // returns 2010
+  // print('3' + 3); value of type 'int' can'nt be assigned to a variable of type 'string'
+  // printing a string value number of times
+  print('pratick ' * 5); // helps to print values for desired number of times
+  
 }
