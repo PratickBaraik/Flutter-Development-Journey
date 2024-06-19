f1e6f5edc28d35792b5d5c31d914598e0364cc41
PratickB111 <animeNerd.int@protonmail.com>
2024-06-19 21:26:22 +0530

    added some codes

---
+++ day 11-20/constants.dart
@@ -0,0 +1,16 @@
+main() {
+  const intConst = 1234;
+  const strConst = 'Pratick';
+  const doubleConst = 3.14159;
+
+  // printing constants
+  print("Integer constant: $intConst");
+  print("String constant: $strConst");
+  print("Double constant: $doubleConst");
+
+  // printing the type values of variable during runtime
+  print("Runtime type of intConst: ${intConst.runtimeType}");
+  print("Runtime type of strConst: ${strConst.runtimeType}");
+  print("Runtime type of doubleConst: ${doubleConst.runtimeType}");
+
+}
--- day 11-20/type_cast.dart
+++ day 11-20/type_cast.dart
@@ -1,15 +1,26 @@
-// not importing the dafault import dart:core package
-
 main() {
   // string to int
   String myIntStr = '1234';
   int convertedInt = int.parse(myIntStr);
   print('Original string: $myIntStr');
   print('Converted string to int: $convertedInt\n');
+  assert(myIntStr == 1234);
 
   // string to double
   String myDoubleStr = '44.32';
   double convertedDouble = double.parse(myDoubleStr);
   print('Original string: $myDoubleStr');
   print('Converted string to double: $convertedDouble\n');
+
+  // second way
+  String intToStr = 124.toString();
+  if (intToStr is String) {
+    print(intToStr);
+  } else {
+    print("Next time!");
+  }
+
+  String doubleToStr = 3.14159.toStringAsFixed(3);
+  assert(doubleToStr == '3.141');
+  print(doubleToStr);
 }
