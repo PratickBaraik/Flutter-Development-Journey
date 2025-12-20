b154aec962919c29624948e62be2d4259f5d43da
PratickBaraik <animeNerd.int@protonmail.com>
2025-12-20 23:11:17 +0530

    home page app bar and search bar done!

---
+++ flutter_demo12_ecommerce_app/lib/pages/HomePage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo12_ecommerce_app/widgets/HomeAppBar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          HomeAppBar(),
+          Container(
+            // adding temporary height
+            height: 500,
+            padding: const EdgeInsets.only(top: 15),
+            decoration: const BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search here...'),
+                        ),
+                      ),
+                      const Spacer(),
+                      const Icon(
+                        Icons.camera_alt,
+                        size: 30,
+                        color: Color(0xFF4C53A5),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
