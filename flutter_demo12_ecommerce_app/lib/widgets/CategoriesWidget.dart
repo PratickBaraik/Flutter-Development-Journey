d8eed1368241b9894f985cf51f1ec28a86a9ab02
Pratick Baraik <animeNerd.int@protonmail.com>
2025-12-26 21:44:13 +0530

    categories widget flow created

--- flutter_demo12_ecommerce_app/lib/widgets/CategoriesWidget.dart
+++ flutter_demo12_ecommerce_app/lib/widgets/CategoriesWidget.dart
@@ -9,6 +9,7 @@ class CategoriesWidget extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
+        for(int i = 1; i <= 6; i++)
           Container(
               margin: const EdgeInsets.symmetric(horizontal: 10),
               padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
@@ -16,12 +17,25 @@ class CategoriesWidget extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: const Row(
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  
+                  Image.asset(
+                    "images/$i.jpg", 
+                    width: 30, 
+                    height: 30,
+                  ), 
+                  Text(
+                    "First",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ),
                 ],
-              )),
+              ),
+            ),
         ],
       ),
     );
