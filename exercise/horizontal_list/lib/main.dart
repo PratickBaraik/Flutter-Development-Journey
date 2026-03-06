455d0f25b857ea44ed444d338fadaca15ec5d8c4
PratickBaraik <animeNerd.int@protonmail.com>
2026-03-06 22:35:35 +0530

    made a simple dart horizontal list - practice coding

--- exercise/horizontal_list/lib/main.dart
+++ exercise/horizontal_list/lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const title = 'Basic List';
+
     return MaterialApp(
-      title: "Horizontal List",
-      theme: ThemeData(primaryColor: Colors.blueAccent),
+      debugShowCheckedModeBanner: false,
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(title)),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(leading: Icon(Icons.map), title: Text('Map')),
+            ListTile(leading: Icon(Icons.photo_album), title: Text('Album')),
+            ListTile(leading: Icon(Icons.phone), title: Text('Phone')),
+            ListTile(
+              leading: Icon(Icons.verified_user_outlined),
+              title: Text('User 1'),
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user_outlined),
+              title: Text('User 2'),
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user_outlined),
+              title: Text('User 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
